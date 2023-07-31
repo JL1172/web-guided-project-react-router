43c0abd5dc70f969d6c4ee5ed03e029d39a15d71
Jacob Lang <jacoblang72@comcast.net>
2023-07-31 09:31:45 -0400

    done

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,9 +1,10 @@
 import React from 'react'
 // We'll need a Link from 'react-router-dom'
+import {Link} from 'react-router-dom';
 
 export default function ItemsList(props) {
   const { items } = props
-
+console.log(items)
   return (
     <div className='items-list-wrapper'>
       {items.map(item => (
@@ -12,14 +13,14 @@ export default function ItemsList(props) {
           key={item.id}
         >
           {/* 👉 STEP 6 - Link starts, navigates us from <current url> to <current url>/<id of the item> */}
-          <>
+          <Link to = {`${item.id}`}>
             <img
               className='items-list-image'
               src={item.imageUrl}
               alt={item.name}
             />
             <p>{item.name}</p>
-          </>
+          </Link>
           {/* Link ends */}
 
           <p>${item.price}</p>
